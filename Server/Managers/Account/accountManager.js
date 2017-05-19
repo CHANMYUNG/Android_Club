b661902a36edde49f0e7f21376bdca365d6d38ee
Terry Yun <nooheat1228@gmail.com>
2017-05-19 23:23:18 +0900

    USE JSON

--- Server/Managers/Account/accountManager.js
+++ Server/Managers/Account/accountManager.js
@@ -3,92 +3,136 @@ let model = require('./accountModel');
 
 let Manager = {};
 
-Manager.login = function( id, password, callback){
-    model.find({"id":id,"password":password}, function(err,results){
-        if(err){
-            callback(err,null);
-            return;
+Manager.login = function (id, password, callback) {
+    model.find({ "id": id, "password": password }, function (err, results) {
+
+        let response = {
+            "error": false,
+            "success": false
+        };
+        let JSONResponse;
+
+        if (err) {
+            response.error = true;
         }
 
-        if(results.length>0){
-            callback(null,results[0]);
+        else if (results.length > 0) {
+            response.success = true;
         }
-        else{
-            callback(null,null);
+        else {
+            response.success = false;
         }
+
+        JSONResponse = JSON.stringify(response);
+        callback(JSONResponse);
     });
 };
 
-Manager.isIdExist = function(id, callback){
-    model.find({"id":id}, function(err,results){
-        if(err){
-            callback(err, null);
-            return;
-        }
-        
-        if(results.length > 0 ){
-            callback(null, true);
+Manager.isIdExist = function (id, callback) {
+    model.find({ "id": id }, function (err, results) {
+
+        let response = {
+            "error": false,
+            "exist": false
+        };
+        let JSONResponse;
+
+        if (err) {
+            response.error = true;
         }
 
-        else{ 
-            callback(null, false);
+        else if (results.length > 0) {
+            response.exist = true;
         }
-    })
-};
+        else {
+            response.exist = false;
+        }
 
-Manager.isStuExist = function(stuNum, callback){
-    model.find({"stuNum" : stuNum}, function(err, results){
-        if(err){
-            callback(err, null);
-            return;
-        }
-        if(results.length > 0 ){
-            callback(null, true);
-        }
-        else{ 
-            callback(null, false);
-        }
+        JSONResponse = JSON.stringify(response);
+        callback(JSONResponse);
     });
 };
 
-Manager.signUp = function(object, callback){
+Manager.isStuExist = function (stuNum, callback) {
+    model.find({ "stuNum": stuNum }, function (err, results) {
+
+        let response = {
+            "error": false,
+            "exist": false
+        };
+        let JSONResponse;
+
+        if (err) {
+            response.error = true;
+        }
+
+        else if (results.length > 0) {
+            response.exist = true;
+        }
+        else {
+            response.exist = false;
+        }
+
+        JSONResponse = JSON.stringify(response);
+        callback(JSONResponse);
+    });
+};
+
+Manager.signUp = function (object, callback) {
 
     let data = new model(object);
-    data.save(function(err){
-        if(err){
-            callback(true);
+    let response = {
+        "error": false,
+        "success": false
+    };
+    let JSONResponse;
+    
+    data.save(function (err) {
+        if (err) {
+            response.error = true;
         }
-	    else{        
-	        callback(false);
-    	}
+        else {
+            response.success = true;
+        }
+    });
+
+    JSONResponse = JSON.stringify(response);
+    callback(JSONResponse);
+};
+
+Manager.findAccountByNum = function (stuNum, callback) {
+    model.find({ "stuNum": stuNum }, function (err, results) {
+        let response = {
+            "error" : false,
+            "account" : {}
+        }
+
+        let JSONResponse;
+        if (err) {
+            response.error = true;
+        }
+        if (results.length > 0) {
+            response.account = results[0];
+        }
+        else {
+            response.account = null;
+        }
+
+        JSONResponse = JSON.stringify(response);
+        callback(JSONResponse)
     });
 };
 
-Manager.findByNum = function(stuNum, callback){
-    let result = model.find({"stuNum" : stuNum}, function(result){
-        if(err){
+Manager.findById = function (id) {
+    let result = model.find({ "id": id }, function (result) {
+        if (err) {
             callback(-1);
             return;
         }
-        if(results.length > 0){
+        if (results.length > 0) {
             callback(JSON.stringify(results[0]));
         }
-        else{
-            callback(null);
-        }
-    });
-};
-
-Manager.findById = function(id){
-    let result = model.find({"id" : id}, function(result){
-        if(err){
-            callback(-1);
-            return;
-        }
-        if(results.length > 0){
-            callback(JSON.stringify(results[0]));
-        }
-        else{
+        else {
             callback(null);
         }
     });
