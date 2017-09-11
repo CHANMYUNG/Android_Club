212d7af8d8968f783914d3f9b38953e80f542018
yjh5424 <yjh54240@naver.com>
2017-09-11 17:06:06 +0900

    오류수정

--- Android_Club/app/src/main/java/com/example/www/android_club/activities/MainActivity.java
+++ Android_Club/app/src/main/java/com/example/www/android_club/activities/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=(RecyclerView)findViewById(R.id.main_recyclerView);
